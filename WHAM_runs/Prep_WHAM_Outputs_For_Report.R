b5bcac8056bef39aae34bc3b237a85fd301ddac7
kcurti <kiersten.curti@noaa.gov>
2022-09-13 13:10:41 -0400

    Update Prep_WHAM_Outputs_For_Report.R
    progres

--- WHAM_runs/Prep_WHAM_Outputs_For_Report.R
+++ WHAM_runs/Prep_WHAM_Outputs_For_Report.R
@@ -18,17 +18,21 @@ prev.run.name <- "WHAM_Run29F4"
 
 
 ### Read in current model, projection and input files
+
 model.rds <- paste(run.name, "basic_model.rds", sep="_")
 input.rds <- paste(run.name, "input.rds", sep="_")
 f40.rds <- paste(run.name, "Proj_F40.rds", sep='.')
 
 WHAM_basic <- readRDS(file.path(run.dir, model.rds))
 input <- readRDS(file.path(run.dir, input.rds))
-proj40 <- readRDS(file.path(run.dir, f40.rds))
+proj40 <- readRDS(file.path(run.dir, "projections/F40",f40.rds))
+
 
 
 ### Model specs
+
 model.yrs <- input$years
+model.lyr <- tail(model.yrs,1)
 model.nyrs <- length(model.yrs)
 nage <- input$data$n_ages
 ages.labels <- input$ages.lab
@@ -37,10 +41,33 @@ ages.labels <- input$ages.lab
 
 ### Current Model estimates
 
-
 # Estimates and standard deviations from sdreport
 WHAM_basic.ests <- as.list(WHAM_basic$sdrep, what = "Est", report = TRUE)
 WHAM_basic.sd <- as.list(WHAM_basic$sdrep, what = "Std", report = TRUE)
+
+
+# Reference points
+Fproxy <- as.vector(exp(WHAM_basic.ests$log_FXSPR_static))
+  names(Fproxy) <- 'est'
+
+SSBproxy.vec <- data.frame(log.est = WHAM_basic.ests$log_SSB_FXSPR_static,
+                           log.se  = WHAM_basic.sd$log_SSB_FXSPR_static) %>%
+  mutate( est = exp(log.est),
+          lo = exp(log.est - qnorm(0.975)*log.se),
+          hi = exp(log.est + qnorm(0.975)*log.se)
+  )
+
+MSYproxy.vec <- data.frame(log.est = WHAM_basic.ests$log_Y_FXSPR_static,
+                           log.se  = WHAM_basic.sd$log_Y_FXSPR_static) %>%
+  mutate( est = exp(log.est),
+          lo = exp(log.est - qnorm(0.975)*log.se),
+          hi = exp(log.est + qnorm(0.975)*log.se)
+  )
+
+brps <- bind_rows('Fproxy'=Fproxy, 'SSBproxy'=SSBproxy.vec, 'MSYproxy'=MSYproxy.vec, .id='BRP') %>%
+  select(c(BRP,est,lo,hi))
+SSBproxy <- SSBproxy.vec$est
+MSYproxy <- MSYproxy.vec$est
 
 
 # Abundance and F at age
@@ -61,7 +88,8 @@ F.yr <-
   ) %>%
   mutate( est = exp(log.est),
           lo = exp(log.est - qnorm(0.975)*log.se),
-          hi = exp(log.est + qnorm(0.975)*log.se)
+          hi = exp(log.est + qnorm(0.975)*log.se),
+          relF = est/Fproxy
   )
 
 
@@ -73,7 +101,8 @@ SSB.yr <-
   ) %>%
   mutate( est = exp(log.est),
           lo = exp(log.est - qnorm(0.975)*log.se),
-          hi = exp(log.est + qnorm(0.975)*log.se)
+          hi = exp(log.est + qnorm(0.975)*log.se),
+          relSSB = est/SSBproxy
   )
 
 
@@ -87,40 +116,6 @@ Rect <-
           lo = exp(log.est - qnorm(0.975)*log.se),
           hi = exp(log.est + qnorm(0.975)*log.se)
   )
-
-
-# Reference points
-Fproxy <- exp(WHAM_basic$rep$log_FXSPR_static)
-  names(Fproxy) <- 'est'
-
-SSBproxy.vec <- data.frame(log.est = WHAM_basic$rep$log_SSB_FXSPR_static,
-                           log.se  = as.list(WHAM_basic$sdrep, what = "Std", report = TRUE)$log_SSB_FXSPR_static) %>%
-  mutate( est = exp(log.est),
-          lo = exp(log.est - qnorm(0.975)*log.se),
-          hi = exp(log.est + qnorm(0.975)*log.se)
-  )
-
-MSYproxy.vec <- data.frame(log.est = WHAM_basic$rep$log_Y_FXSPR_static,
-                           log.se  = as.list(WHAM_basic$sdrep, what = "Std", report = TRUE)$log_Y_FXSPR_static) %>%
-  mutate( est = exp(log.est),
-          lo = exp(log.est - qnorm(0.975)*log.se),
-          hi = exp(log.est + qnorm(0.975)*log.se)
-  )
-
-brps <- bind_rows('Fproxy'=Fproxy, 'SSBproxy'=SSBproxy.vec, 'MSYproxy'=MSYproxy.vec, .id='BRP') %>%
-  select(c(BRP,est,lo,hi))
-SSBproxy <- SSBproxy.vec$est
-MSYproxy <- MSYproxy.vec$est
-
-
-# Relative F and SSB
-relF <- F.yr %>%
-  mutate(relF = est/Fproxy) %>%
-  select(Year, est, relF)
-
-relSSB <- SSB.yr %>%
-  mutate(relSSB = est/SSBproxy) %>%
-  select(Year, est, relSSB)
 
 
 
@@ -142,8 +137,7 @@ prev.MSYproxy <- exp(prev.WHAM_basic$rep$log_Y_FXSPR_static)
 
 
 
-#### Text for BRP table in assessment report
-
+### Text for BRP table in assessment report
 
 # Function to create the brp text
 create.brp.text <- function(brp.name, round.digits)
@@ -172,8 +166,6 @@ f.text <- create.brp.text('Fproxy', 2)
 
 
 ### Current projections
-
-proj40 <- readRDS(file.path(run.dir,"projections/F40", f40.rds))
 
 # # Terminal year estimates and relative status
 # relF.termyr <- relF[model.lyr,]
@@ -220,6 +212,20 @@ proj40 <- readRDS(file.path(run.dir,"projections/F40", f40.rds))
 
 
 
-
+# ### Write csv files
+# 
+# if(proj.name == 'F40')
+# {
+#   # Model summary
+#   write.csv(rel.status, file.path(res_dir, "Relative.stock.status.csv"))
+#   write.csv(termyr.ests, file.path(res_dir, "Terminal.yr.ests.csv"))
+#   write.csv(brps, file.path(res_dir, "Reference.points.csv"), row.names = FALSE)
+# 
+#   write.csv(ssb.allyrs[model.yrs,], file.path(res_dir, "SSB.ests.csv"))
+#   write.csv(Fmax.allyrs[model.yrs,], file.path(res_dir, "F.ests.csv"))
+#   write.csv(catch.allyrs[model.yrs,], file.path(res_dir, "Observed.catch.csv"))
+#   write.csv(Rect.allyrs[model.yrs,], file.path(res_dir, "Rect.ests.csv"))
+# }
+# 
 
 
