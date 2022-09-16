1428fe342d84bf35ff53636c0671b2bcfc93ac6e
kcurti <kiersten.curti@noaa.gov>
2022-09-16 13:21:08 -0400

    Create README.R
    initial commit

---
+++ README.R
@@ -0,0 +1,30 @@
+### Generic program order
+
+#### Code specific to the run
+
+# Run models
+Run4_WHAM_MT2022.Rmd
+
+# Run projections
+Run4_Projections_WHAM_MT2022.R
+
+
+#### Results code
+
+# Analyze the results from the basic WHAM model; note that this code can also be used for the OSA run
+Analyze.WHAM_basic.Outputs.R
+
+# Analyze projection output
+Analyze.WHAM.projection.output.R
+
+# Prepare results for the report
+# Need to be on network for the last two lines of the code to work (where the files are saved on your network drive_)
+Prep_WHAM_Outputs_For_Report.R
+  
+
+#### Report code
+# Report code is on the network
+MakeUpdateDoc_Server1.r
+
+
+
